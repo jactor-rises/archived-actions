fc52e9b503ca1dcd354c58606a5a6b6917ba24bb
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2019-12-01 19:48:19 +0100

    new git user name for tag

--- release-git-tag/tag.sh
+++ release-git-tag/tag.sh
@@ -6,6 +6,6 @@ TAGGED_CONTENT=$(cat $TAG_FILE_NAME)
 echo "Creating tag: $TAGGED_CONTENT"
 
 git config --global user.email "tor.egil.jacobsen@gmail.com"
-git config --global user.name "release-prepare-mvn-artifact GitHub Action"
+git config --global user.name "release-git-tag GitHub Action"
 
 git tag -a ${TAGGED_CONTENT} -m "$GITHUB_ACTOR released artifact: $TAGGED_CONTENT"
