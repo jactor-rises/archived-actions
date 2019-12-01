26fb938c3d3e3a5fe9e2262f063f37ca693cd710
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2019-12-01 19:25:27 +0100

    adds git global config for git tag

--- release-git-tag/dist/tag.sh
+++ release-git-tag/dist/tag.sh
@@ -5,4 +5,9 @@ TAGGED_CONTENT=$(cat $TAG_FILE_NAME)
 
 echo "Creating tag: $TAGGED_CONTENT"
 
-git tag -a ${TAGGED_CONTENT} -m "released artifact: $TAGGED_CONTENT"
+env
+
+git config --global user.email "tor.egil.jacobsen@gmail.com"
+git config --global user.name "release-prepare-mvn-artifact GitHub Action"
+
+git tag -a ${TAGGED_CONTENT} -m "$GITHUB_ACTOR released artifact: $TAGGED_CONTENT"
