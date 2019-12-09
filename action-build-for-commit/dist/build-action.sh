03e56ab2b5568878b7c38d18743265ce74883950
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2019-12-09 20:30:10 +0100

    INPUT_ACTION_FOLDER -> INPUT_ACTION-FOLDER

--- action-build-for-commit/dist/build-action.sh
+++ action-build-for-commit/dist/build-action.sh
@@ -4,9 +4,9 @@ set -e
 echo "debug workspace"
 env
 
-echo "Building action: $INPUT_ACTION_FOLDER"
+echo "Building action: $INPUT_ACTION-FOLDER"
 
-cd "$RUNNER_WORKSPACE/actions/$INPUT_ACTION_FOLDER"
+cd "$RUNNER_WORKSPACE/actions/$INPUT_ACTION-FOLDER"
 
 npm install
 ncc build index.js
